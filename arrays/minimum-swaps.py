62b569f08fe47d5e103ad544be065ab3a8758434
Karthik M <chynkm@gmail.com>
2021-07-05 21:15:36 +0530

    2055 - 2116 Completed solution for min swaps (0.35)

--- arrays/minimum-swaps.py
+++ arrays/minimum-swaps.py
@@ -1,15 +1,17 @@
 def minimumSwaps(arr):
     swaps = 0
-    a = dict(enumerate(arr, 1))
-    for k, v in a.items():
-        if k != v:
-            a[v], a[k] = v, a[v]
+    for i in range(len(arr)):
+        while arr[i] != i+1:
+            temp = arr[i]
+            arr[i] = arr[temp - 1]
+            arr[temp - 1] = temp
             swaps += 1
 
     return swaps
 
 
+a = [4, 3, 1, 2]
 a = [2, 3, 4, 1, 5]
-# a = [1, 3, 5, 2, 4, 6, 7]
-# a = [7, 1, 3, 2, 4, 5, 6]
+a = [1, 3, 5, 2, 4, 6, 7]
+a = [7, 1, 3, 2, 4, 5, 6]
 print(minimumSwaps(a))
